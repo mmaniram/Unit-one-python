85a5e17bc4f08d953ee3793a672550c798b51f48
M <mmaniram@brooklynsteamcenter.org>
2023-09-19 10:53:33 -0400

    unit 1 python

---
+++ benchmark.py
@@ -0,0 +1,41 @@
+#Exercise 
+print ("Mrs. Raghunauth")
+
+#Exercise 2
+FavoriteShow = "Bollywood"
+
+
+#Exercise 3
+Height = 5
+Age = 40
+print (Height * Age)
+
+#Exercise 4
+print ("I am going to break")
+
+#Excersice 5
+Fav_food=[' pizza','noodles', 'seafood'  ]
+
+#exercise 6 
+if Age >=21 :
+ print('You can rent a car')
+else : 
+    print("you're beat")
+#Exercise 7
+    input ("what is your name?")
+    print('Hello, " + Their_Name ')
+
+ # Exercise 8
+for i in Fav_food :
+    print(i)
+
+    #exercise 9 
+    x = 1
+    while x == 2 : 
+        print("equal>:)")
+
+
+
+
+
+
